d4207fd0a613df76e710884311ff6f63bbe88877
Gary-yp <imcore@xieyanping.local>
2014-06-11 15:40:07 +0800

    work_one first commit

---
+++ work_one/work_one/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  work_one
+//
+//  Created by 02 on 14-6-11.
+//  Copyright (c) 2014年 02. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
